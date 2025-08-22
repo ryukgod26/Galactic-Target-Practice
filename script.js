2f9cea7d3594133f08897d9c8afc6bd79633b450
raviwhitehat <ekravi26@gmail.com>
2025-08-22 22:11:12 +0530

    Enhance enemy behavior and add player component; update models and physics settings

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ AFRAME.registerComponent('enemy', {
     schema: {
         health:{type:'number',default:100},
         damage:{type:'number',default:5},
-        speed:{type:'number',default:5},
+        speed:{type:'number',default:3},
         target:{type:'selector',default:'#player'}
 
     },
 
     init: function () {
 
-        let el = this.el;
+
         let data = this.data;
         this.speed = this.data.speed;
         this.damage = this.data.damage;
-        this.target = this.data.target;
+
         this.directionVec3 = new THREE.Vector3();
- this.el.addEventListener('click', this.onClick);
+ this.el.addEventListener('click', this.onClick.bind(this));
+ this.el.addEventListener('body-loaded', () => {
+  const body = this.el.body
+  if (! body) return;
+
+  // I learned this from AI.
+  body.linearDamping = 0.05;
+  body.angularDamping = 0.9;
+  console.log('Enemy Ready');
+  
+});
 console.log(data);
 
     },
@@ -24,8 +34,13 @@ console.log('Enemy Clicked!!!');
 this.data.health -= 25;
 if(this.data.health <= 0){
     console.log('Enemy Destroyed!!!');
-    this.el.setAttribute('visible','false');
+    this.el.setAttribute('visible',false);
     this.el.emit('enemy-die');
+
+    if (this.el.body) {
+        this.el.body.setLinearVelocity(new Ammo.btVector3(0,0,0));
+        this.el.body.setAngularVelocity(new Ammo.btVector3(0,0,0));
+      }
 }
 else{
     console.log(`Enemy Health is ${this.data.health}`);
@@ -39,30 +54,68 @@ else{
 
     remove: function () {
  this.el.removeEventListener('click', this.onClick);
+
  },
 
     tick: function (time, timeDelta) {
-      let dirV3 = this.directionVec3;
-      let targetPosition = this.data.target.object3D.position;
-      let currentPosition = this.el.object3D.position;
+    
+const body = this.el.body;
+    if (!this.data.target) return;
+  if (!this.el.body) return; 
+  const targetPos = this.data.target.object3D.position;
+  const pos = this.el.object3D.position;
+  this.directionVec3.set(
+    targetPos.x - pos.x,
+    targetPos.y - pos.y,
+    targetPos.z - pos.z
+  );
+  const dist = this.directionVec3.length();
+  if(dist < 0.3){
+    //SLowing Near Player
+    body.velocity.x *= 0.8;
+    body.velocity.y *= 0.8;
+    body.velocity.z *= 0.8;
+    console.log("Enemy Has Reached Near the Player");
+    
+    return ;
+  }
+  body.velocity.x = this.directionVec3.x;
+  body.velocity.y = this.directionVec3.y;
+  body.velocity.z = this.directionVec3.z;
 
-      dirV3.copy(targetPosition).sub(currentPosition);
 
-      var dist = dirV3.length();
 
-      if(dist < 1){
-        console.log('Enemy has Reached the Player.')
-        return ;
-      }
- // Scale the direction vector's magnitude down to match the speed.
-      let factor = this.speed/dist;
-      ['x','y','z'].forEach(function(axis){
-        dirV3[axis] *= factor * (timeDelta/1000);
-      });
-      this.el.setAttribute('position',{
-        x: currentPosition.x + dirV3.x,
-        y: currentPosition.y + dirV3.y,
-        z: currentPosition.z + dirV3.z
-      })
     }
+});
+
+//Player
+AFRAME.registerComponent('player', {
+  schema: {
+    score:{type:'number',default:0},
+    health:{type:'number',default:100},
+    speed:{type:'number',default:30}
+  },
+
+  init: function () {
+    this.el.addEventListener('enemy-die',this.enemyDie);
+    this.el.addEventListener('collide',function(event){
+console.log('Player has collided with ',event.detail.body.el);
+    })
+  },
+
+  enemyDie:function(){
+    this.data.score +=1 ;
+    console.log(this.data.score);
+  },
+  update: function () {
+    // Do something when component's data is updated.
+  },
+
+  remove: function () {
+    // Do something the component or its entity is detached.
+  },
+
+  tick: function (time, timeDelta) {
+    // Do something on every scene tick or frame.
+  }
 });
